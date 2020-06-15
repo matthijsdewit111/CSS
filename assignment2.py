ba2f9367f4510759c3a8495beb77c1c5ffb118d9
Matthijs de Wit <matthijs111@gmail.com>
2020-06-15 15:14:56 +0200

    reformat

--- assignment2.py
+++ assignment2.py
@@ -1,22 +1,23 @@
 # Assignment 2 part 1
 # Coen Lenting
 
-import numpy as np
-from copy import deepcopy
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-import time
-from matplotlib import cm
-from matplotlib.colors import ListedColormap, LinearSegmentedColormap
-import random
 import pickle
+import random
+import time
+from copy import deepcopy
+
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import cm
+from matplotlib.colors import LinearSegmentedColormap, ListedColormap
 
 t1 = time.time()
 
 
 class C():
     # the diffusion class. Owns a C.c which is the matrix with all the information
-    def __init__(self, seed, eps = 10**-14, N = 50, D = 1, w = 1.91, eta = 1):
+    def __init__(self, seed, eps=10**-14, N=50, D=1, w=1.91, eta=1):
         self.N = N
         self.dx = 1/N
         self.D = D
@@ -25,16 +26,16 @@ class C():
         self.eta = eta
         self.eps = eps
         self.converged = False
-        self.cluster = [[0 for i in range(N)] for j in range(N)] # 0 is no cluster 1 is
+        self.cluster = [[0 for i in range(N)] for j in range(N)]  # 0 is no cluster 1 is
         self.cluster[seed[1]][seed[0]] = 1
         self.clusters = {tuple([seed[1], seed[0]])}
-        self.candidates = {tuple([seed[1], seed[0]])} # (y, x) from top left
-        
+        self.candidates = {tuple([seed[1], seed[0]])}  # (y, x) from top left
+
     def update(self):
         # update the matrix
         deltamax = 0
-        for i in range(1, self.N - 1): # y-axis
-            for j in range(self.N): # x-axis
+        for i in range(1, self.N - 1):  # y-axis
+            for j in range(self.N):  # x-axis
                 if self.cluster[i][j] == 0:
                     if (j - 1) < 0:
                         jm1 = self.N - 1
@@ -55,11 +56,11 @@ class C():
                         deltamax = delta
                 else:
                     self.c[i][j] = 0
-                    
+
             # check for convergence
             if deltamax < self.eps:
                 self.converged = True
-            
+
     # compute the growth candidates
     def growth_candidates(self):
         g_candidates = set()
@@ -73,7 +74,7 @@ class C():
                 g_candidates.add(tuple([i - 1, j]))
             if (i + 1 <= (self.N - 1)):
                 g_candidates.add(tuple([i + 1, j]))
-            
+
         self.candidates = self.candidates | g_candidates
 
     # take a growth step
@@ -84,11 +85,10 @@ class C():
         c_sum = 0
         combined_c = 0
         rndm = random.random()
-        
+
         # compute total concentration
         for i, j in self.candidates:
             c_sum += self.c[i][j] ** self.eta
-
 
         # compute the growth site
         for i, j in self.candidates:
@@ -100,8 +100,6 @@ class C():
         self.converged = False
 
 
-
-
 N = 100
 etas = [0.5, 1, 1.5, 2]
 fig, axs = plt.subplots(2, 2)
@@ -110,24 +108,24 @@ cntr = 0
 
 # compute DLA for all eta's
 for eta in etas:
-    c = C(seed = [N//2, N - 1], N = N, eta = eta)
+    c = C(seed=[N//2, N - 1], N=N, eta=eta)
     while (c.converged == False):
         c.update()
     c.eta = eta
-    
+
     for i in range(250):
         if i % 10 == 0:
             print(i)
         c.growth()
-        
+
         while (c.converged == False):
             c.update()
-            
+
     for i in range(N):
         for j in range(N):
             if c.cluster[i][j] == 1:
                 c.c[i][j] = float('nan')
-                
+
     im = axs[cntr].imshow(c.c)
     axs[cntr].set_title("eta = " + str(eta))
     axs[cntr].set_xlabel("x position [-]")
@@ -135,12 +133,7 @@ for eta in etas:
     cntr += 1
 
 
-            
-fig.colorbar(im, ax = axs)
+fig.colorbar(im, ax=axs)
 t2 = time.time()
 print(t2-t1, "TIME")
 plt.show()
-
-
-
-
