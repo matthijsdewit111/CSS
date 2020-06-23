356fdb4ae2922e32d64781ba31ec1209bb9bd707
Matthijs de Wit <matthijs111@gmail.com>
2020-06-23 23:16:30 +0200

    fixed bug where growth candidates where not updated enough

--- code/randomwalk_3d.py
+++ code/randomwalk_3d.py
@@ -97,7 +97,7 @@ class C():
 
         # create list of walkers
         walker_p = []
-        for w in range(1):
+        for w in range(10):
             rndm1 = random.randrange(self.N)
             rndm2 = random.randrange(self.N)
             walker = [rndm2, 0, rndm1]
@@ -129,6 +129,7 @@ class C():
                             x = random.random()
                             if x > p_stick:
                                 self.tree.add([i, j, k], creation_time)
+                                candidates = self.tree.growth_candidates()
                                 no_match = False
                                 self.walking = False
                                 break
@@ -146,7 +147,7 @@ p_stick = 0
 c = C(seed=[N//2, N - 1, N//2], N=N)
 
 # number of points
-for i in tqdm(range(600)):
+for i in tqdm(range(200)):
     while (c.walking == True):
         c.walker(p_stick, i + 1)
     c.walking = True
